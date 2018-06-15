343150dfc96a6e9cde64a46e7197e8a0cb466594
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-06-15 03:53:42 +0000

    follow, unfollow

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -14,7 +14,7 @@ class RegisterController extends Controller
     | Register Controller
     |--------------------------------------------------------------------------
     |
-    | This controller handles the registration of new users as well as their
+    | This contro1ler handles the registration of new users as well as their
     | validation and creation. By default this controller uses a trait to
     | provide this functionality without requiring any additional code.
     |
